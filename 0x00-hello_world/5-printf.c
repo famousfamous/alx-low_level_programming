4193cb2bca89a1e3a96aeb6cdcd0efe105365d48
famousfamous <writefamous@gmail.com>
2023-02-16 11:20:03 -0800

    with proper grammar

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - a programme that prints a line with printf function.
+ * Return: 0.
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art\,");
+return (0);
+}
