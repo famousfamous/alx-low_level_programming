ac4344f73b5f2e1f583d396f9ac8e6cdf1ceeed1
famousfamous <writefamous@gmail.com>
2023-02-17 06:10:19 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - using the main function
+ * Return: 0
+ */
+int main(void)
+{
+char alpha[26] = "abcdefghijklmnopqrstuvwxyz";
+int i;
+
+for (i = 0; i < 26; i++);
+{
+putchar(alpha[i]);
+}
+putchar('\n');
+return (0);
+}
